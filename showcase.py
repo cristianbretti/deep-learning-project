94a8b5b62b8f0b09a6e4ba2fdebb281ce83e815f
Anton Stagge <antonstagge95@gmail.com>
2019-05-07 18:54:07 +0200

    Showcase more images at the same time

--- showcase.py
+++ showcase.py
@@ -6,13 +6,13 @@ from skimage import color
 from colorizer import Colorizer, DatasetIterator
 
 if __name__ == "__main__":
-    orignal_image_folder = 'datasets/preprocessed'
+    orignal_image_folder = 'datasets/preprocessed_old'
 
     filenames = [os.path.join(orignal_image_folder, record) for record in os.listdir(
         orignal_image_folder) if os.path.isfile(os.path.join(orignal_image_folder, record))]
 
     n_epochs = 1
-    batch_size = 1
+    batch_size = 10
 
     iterator = DatasetIterator(filenames, n_epochs, batch_size, shuffle=True)
     colorizer = Colorizer(iterator)
@@ -22,24 +22,31 @@ if __name__ == "__main__":
     with tf.Session() as sess:
         saver.restore(sess, 'models/my-model')
 
-        new_ab, example = sess.run([new_image_node, example_node])
-        new_ab = new_ab.reshape((299, 299, 2))
-        new_ab = (new_ab)*127
+        new_ab_list, example = sess.run([new_image_node, example_node])
 
-        l_channel = example['l_channel'].values.reshape((299, 299, 1))
-        real_ab = example['ab_channels'].values.reshape((299, 299, 2))
-        l_channel = (l_channel + 1)*100/2
-        real_ab = (real_ab)*127
+        l_channel_list = example['l_channel'].values.reshape(
+            (batch_size, 299, 299, 1))
+        real_ab_list = example['ab_channels'].values.reshape(
+            (batch_size, 299, 299, 2))
 
-        lab_image = np.concatenate((l_channel, real_ab), axis=2)
-        real_image = color.lab2rgb(lab_image)
+        for i in range(batch_size):
+            new_ab = new_ab_list[i].reshape((299, 299, 2))
+            new_ab = (new_ab)*127
 
-        new_lab_image = np.concatenate((l_channel, new_ab), axis=2)
-        new_image = color.lab2rgb(new_lab_image)
+            l_channel = l_channel_list[i]
+            real_ab = real_ab_list[i]
+            l_channel = (l_channel + 1)*100/2
+            real_ab = (real_ab)*127
 
-        plt.subplot(121)
-        plt.imshow(real_image)
+            lab_image = np.concatenate((l_channel, real_ab), axis=2)
+            real_image = color.lab2rgb(lab_image)
 
-        plt.subplot(122)
-        plt.imshow(new_image)
-        plt.show()
+            new_lab_image = np.concatenate((l_channel, new_ab), axis=2)
+            new_image = color.lab2rgb(new_lab_image)
+
+            plt.subplot(121)
+            plt.imshow(real_image)
+
+            plt.subplot(122)
+            plt.imshow(new_image)
+            plt.show()
--- train.py
+++ train.py
@@ -10,16 +10,19 @@ if __name__ == "__main__":
     filenames = [os.path.join(orignal_image_folder, record) for record in os.listdir(
         orignal_image_folder) if os.path.isfile(os.path.join(orignal_image_folder, record))]
 
-    n_epochs = 10
+    n_epochs = 1000
     batch_size = 1
 
     iterator = DatasetIterator(filenames, n_epochs, batch_size)
     colorizer = Colorizer(iterator)
     optimizer, loss_node = colorizer.training_op()
 
-    load = False
-    save = False
     saver = tf.train.Saver()
+
+    load = True
+    save = True
+
+    count = 0
     losses = []
     with tf.Session() as sess:
         if load:
